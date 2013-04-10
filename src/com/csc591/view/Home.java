4be03e5610d8480a2a4c008af03dfe9f128f62ba
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2013-04-10 01:09:11 -0400

    Removed default title bar and updated app name
    1. App appears as full screen after removing the default
    2. Changed default app name from Home to Walk North Hill

--- src/com/csc591/view/Home.java
+++ src/com/csc591/view/Home.java
@@ -2,12 +2,20 @@ package com.csc591.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class Home extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
+		// Removes title Bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		// Uses below to Remove notification bar but not required in our application. Helpful in games
+		//this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_home);
 	}
 
