70dfce7ad6202adb39731eae31a8b25d4c5bf18e
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2013-04-11 17:44:54 -0400

    Added App Icon and image files
    Minor UI change. COULD BE CHANGED IN FUTURE.

--- gen/com/csc591/view/R.java
+++ gen/com/csc591/view/R.java
@@ -144,6 +144,9 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020002;
         public static final int orange=0x7f020003;
         public static final int sky=0x7f020004;
+        public static final int wyc_green=0x7f020005;
+        public static final int wyc_orange=0x7f020006;
+        public static final int wyc_sky=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f04000d;
