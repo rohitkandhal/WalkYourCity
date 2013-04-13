0a3c29fe098d254a5570248a20b02baa282bbb6f
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2013-04-13 13:41:51 -0400

    Last commit missed file.
    Forget to add file in previous commit

--- src/com/csc591/view/FragmentDirection.java
+++ src/com/csc591/view/FragmentDirection.java
@@ -8,15 +8,19 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.csc591.DAL.Destination;
 import com.csc591.utils.JSONParser;
@@ -43,6 +47,7 @@ public class FragmentDirection extends Activity implements onMyLocationChangeHan
 	private LatLng destinationLatLng;
 	private LocationManager myLocationManager;
 	private MyLocationListener myLocationListener;
+	private Boolean isGPSEnabled;
 	
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -52,7 +57,12 @@ public class FragmentDirection extends Activity implements onMyLocationChangeHan
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_directions);
 		
+		this.myLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		this.myLocationListener = new MyLocationListener();
+		this.myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+		
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragmentDirect)).getMap();
+		
 		
 		// Get received destination object
 		Intent receivedIntent = getIntent();
@@ -63,7 +73,7 @@ public class FragmentDirection extends Activity implements onMyLocationChangeHan
 		this.sourceLatLng = new LatLng(getCurrentLocation().getLatitude(), getCurrentLocation().getLongitude());
 		
 		// Get current location either from GPS or Network Provider
-		//this.addPlaceMarkerOnMap(sourceLatLng, null);
+		this.addPlaceMarkerOnMap(sourceLatLng, null);
 				
 		// Destination place marker
 		this.addPlaceMarkerOnMap(destinationLatLng, receivedDestination);
@@ -71,11 +81,11 @@ public class FragmentDirection extends Activity implements onMyLocationChangeHan
 		// Following marker type should be used in case of custom marker
 		//Marker kiel = map.addMarker(new MarkerOptions().position(destinationLatLng).title(receivedDestination.getName()).snippet(receivedDestination.getDescription()).icon(BitmapDescriptorFactory.fromResource(R.drawable.wyc_green)));
 
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(destinationLatLng, 15));
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(sourceLatLng, 15));
 		// Provide default zoom level (i.e. how much map area to bring in focus)
 		map.animateCamera(CameraUpdateFactory.zoomTo(17), 300, null);
 		
-		this.myLocationListener = new MyLocationListener();
+		
 		//drawWalkRoute(dummyLatLng, destinationLatLng);
 	}
 	
@@ -83,14 +93,26 @@ public class FragmentDirection extends Activity implements onMyLocationChangeHan
 	{
 		super.onResume();
 		// start receiving last known location
-		this.myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+		//this.myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+		this.checkAndPromptGPSEnabled();
+		if(isGPSEnabled)
+		{
+			this.sourceLatLng = new LatLng(getCurrentLocation().getLatitude(), getCurrentLocation().getLongitude());
+
+		}
 	}
 	
 	public void onPause()
 	{
 		super.onPause();
 		// Stop GPS tracking when application looses visibility
-		myLocationManager.removeUpdates(myLocationListener);
+		if(isGPSEnabled)
+		{
+			// BUG: Only remove updates when GPS is enabled. Otherwise there is no listener attached at this moment
+			myLocationManager.removeUpdates(myLocationListener);
+		}
+		//myLocationManager.removeUpdates(myLocationListener);
+		isGPSEnabled = false;
 	}
 		
 	/*
@@ -121,8 +143,6 @@ public class FragmentDirection extends Activity implements onMyLocationChangeHan
 	
 	private Location getCurrentLocation()
 	{
-		this.myLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		
 		Location locationGPS = this.myLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 		Location locationNet = this.myLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		
@@ -152,5 +172,61 @@ public class FragmentDirection extends Activity implements onMyLocationChangeHan
 	}
 
 	
+	public void checkAndPromptGPSEnabled()
+	{
+		this.isGPSEnabled = false;
+		
+		// TWO methods to check GPS is enabled or not. Need to check which one works well
+		/*String provider = Settings.Secure.getString(getContentResolver(),
+			      			Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+		
+		if(!provider.equals("")){
+			//GPS Enabled
+			this.isGPSEnabled = true;
+		   }
+		*/
+		try
+        {
+            isGPSEnabled = this.myLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }
+        catch (Exception ex) {}
+		
+		if(isGPSEnabled)
+        {
+            this.myLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this.myLocationListener);
+        }
+        else
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Your GPS is disabled. Would you like to enable it ?")
+                   .setCancelable(false)
+                   .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                       public void onClick(DialogInterface dialog, int id) {
+
+                             //Send user to GPS settings screen
+                             Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                             
+                             //dialog.dismiss();
+                            // getApplicationContext().startActivity(intent);
+                             //startActivityForResult(intent, 1);
+                             FragmentDirection.this.startActivity(intent);
+                        }
+                   })
+                   .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                       public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                       }
+                   });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+	}
+	
+	public void onActivityResult()
+	{
+		// TODO - add code here to check when user returns from enable GPS screen to our app
+		// then is GPS enabled or not
+	}
+
 	
 }
