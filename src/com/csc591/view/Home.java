62260551f753117abff9f1511f7b11bcaf2898c5
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2014-02-10 19:38:07 -0500

    Update Home.java

--- src/com/csc591/view/Home.java
+++ src/com/csc591/view/Home.java
@@ -24,51 +24,15 @@ public class Home extends Activity implements FragmentDestinationInterface, Frag
 		// Removes title Bar
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
-		// Uses below to Remove notification bar but not required in our application. Helpful in games
-		//this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		
-		
-		// THE CODE BELOW TO CHECK IF THE GPS IS ENABLED OR NOT
-		
-		/*
-		final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE ) ;
-
-	    if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-	        buildAlertMessageNoGps();
-	    }
-		*/
 		this.getCurrentDeviceLocation();
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_home);
-		
-
-		//ViewServer.get(this).addWindow(this);
 	}
-	
-	
 	
 	private LocationManager myLocationManager;
 	private MyLocationListener myLocationListener;
 	
-	
-
-  private void buildAlertMessageNoGps() {
-    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-           .setCancelable(false)
-           .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-               public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                   startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-               }
-           })
-           .setNegativeButton("No", new DialogInterface.OnClickListener() {
-               public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                    dialog.cancel();
-               }
-           });
-    final AlertDialog alert = builder.create();
-    alert.show();
 }
 	
 	public void getCurrentDeviceLocation()
@@ -112,50 +76,6 @@ public class Home extends Activity implements FragmentDestinationInterface, Frag
 		myIntent.putExtra("DestinationObject", destination); //Optional parameters
 		Home.this.startActivity(myIntent);
 	}
-	
-	//public void onListItemClickHandler_OLD_NOT_WORKING(String text) {
-		
-		/** Getting the fragment transaction object, which can be used to add, remove or replace a fragment */
-		//FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-		
-		/** Getting the existing detailed fragment object, if it already exists. The fragment object is retrieved by its tag name*Frag*
-		* Getting the orientation ( Landscape or Portrait ) of the screen */
-		//int orientation = getResources().getConfiguration().orientation;
-		
-		/** Landscape Mode */
-		//if(orientation == Configuration.ORIENTATION_PORTRAIT ){
-			/** Getting the fragment manager for fragment related operations */
-			//FragmentManager fragmentManager = getFragmentManager();
-			//Fragment prevFrag = fragmentManager.findFragmentByTag("com.csc591.view.FragmentDestinations");
-			
-			// Remove the existing detailed fragment object if it exists */
-		//	if(prevFrag!=null)
-		//		fragmentTransaction.remove(prevFrag);
-		
-			// Instantiating the fragment FragmnetDirection */
-			//FragmentDirection fragment = new FragmentDirection();
-		
-			// Creating a bundle object to pass the data(the clicked item's position) from the activity to the fragment */
-			//Bundle b = new Bundle();
-		
-			// Setting the data to the bundle object */
-			//b.putInt("position", position);
-		
-			// Setting the bundle object to the fragment */
-			//fragment.setArguments(b);
-		
-			// Adding the fragment to the fragment transaction */
-			//fragmentTransaction.add(R.id.fragmentDirection, fragment);
-
-		//	fragmentTransaction.replace(R.id.fragmentDestinations,fragment);
-			//getFragmentManager().beginTransaction().add(R.id.fragmentDirection, fragment).commit();
-			// Adding this transaction to backstack */
-		//	fragmentTransaction.addToBackStack(null);
-		
-			// Making this transaction in effect */
-			//fragmentTransaction.commit();
-		        
-	//	}
 		
 	public interface OnFooterCategorySelectionChanged{
 		public void onCategoryChangeHandler(ArrayList<Integer> newFlags);
